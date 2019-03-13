c9fd1392382a4699b0c63ac82f2c7edbfb7b7221
jakubbartolomej <jakubkosuth@gmail.com>
2019-03-13 23:54:49 +0100

    refactoring of graph generation code

--- graphs_plotting/main.py
+++ graphs_plotting/main.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import csv
 
-# Plotting
 
 def show_graph(values, label, bins, rang):
     """
@@ -16,40 +15,25 @@ def show_graph(values, label, bins, rang):
     plt.xlabel(label)
     plt.show()
 
-# ECC part
 
-def prepare_ecc_priv_msb_graph(path):
+def prepare_data(path, start_index, stop_index, row_index):
     """
-    Extract MSB from priv key given in CSV.
-    :param path: path to the CSV file
-    :return: extracted data
-    """
-    msb_private_ecc = []
+        Extract data from from given CSV.
+        :param row_index: row index of data which we want to parse
+        :param stop_index: start index of data which we want to parse
+        :param start_index:  end index of data which we want to parse
+        :param path: path to the CSV file
+        :return: extracted data
+        """
+    data = []
 
     # open csv and parse desired values to array
     with open(path, 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=';')
         for row in plots:
-            msb_private_ecc.append(int(row[2].replace(':', '')[0:2], 16))
+            data.append(int(row[row_index].replace(':', '')[start_index:stop_index], 16))
 
-    return msb_private_ecc
-
-
-def prepare_ecc_priv_lsb_graph(path):
-    """
-    Extract LSB from priv key given in CSV.
-    :param path: path to the CSV file
-    :return: extracted data
-    """
-    lsb_private_ecc = []
-
-    # open csv and parse desired values to array
-    with open(path, 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=';')
-        for row in plots:
-            lsb_private_ecc.append(int(row[2].replace(':', '')[-2:], 16))
-
-    return lsb_private_ecc
+    return data
 
 
 def prepare_ecc_priv_times(path):
@@ -67,75 +51,6 @@ def prepare_ecc_priv_times(path):
             times.append(int(row[3], 10)/1000)
 
     return times
-
-# RSA part
-
-def prepare_p_prime_msb(path):
-    """
-    Extract MSB from P prime given in CSV.
-    :param path: path to the CSV file
-    :return: extracted data
-    """
-    msb_p_prime = []
-
-    # open csv and parse desired values to array
-    with open(path, 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=';')
-        for row in plots:
-            msb_p_prime.append(int(row[3].replace(':', '')[2:4], 16))
-
-    return msb_p_prime
-
-
-def prepare_q_prime_msb(path):
-    """
-    Extract MSB from Q prime given in CSV.
-    :param path: path to the CSV file
-    :return: extracted data
-    """
-    msb_q_prime = []
-
-    # open csv and parse desired values to array
-    with open(path, 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=';')
-        for row in plots:
-            msb_q_prime.append(int(row[4].replace(':', '')[2:4], 16))
-
-    return msb_q_prime
-
-
-def prepare_p_prime_lsb(path):
-    """
-    Extract LSB from P prime given in CSV.
-    :param path: path to the CSV file
-    :return: extracted data
-    """
-    lsb_p_prime = []
-
-    # open csv and parse desired values to array
-    with open(path, 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=';')
-        for row in plots:
-            lsb_p_prime.append(int(row[3].replace(':', '')[-2:], 16))
-
-    return lsb_p_prime
-
-
-def prepare_q_prime_lsb(path):
-    """
-    Extract LSB from Q prime given in CSV.
-    :param path: path to the CSV file
-    :return: extracted data
-    """
-    lsb_q_prime = []
-
-    # open csv and parse desired values to array
-    with open(path, 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=';')
-        for row in plots:
-            lsb_q_prime.append(int(row[4].replace(':', '')[-2:], 16))
-
-    return lsb_q_prime
 
 
 def prepare_rsa_times(path):
@@ -156,86 +71,118 @@ def prepare_rsa_times(path):
 
 
 if __name__ == '__main__':
-    # ECC MSB priv
-    # show_graph(prepare_ecc_priv_msb_graph('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'MSB of priv key', 500, None)
-    # show_graph(prepare_ecc_priv_msb_graph('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'MSB of priv key', None, None)
+    # ECC MSB X
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 0, 2, 1),
+               'MSB of point X', 500, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 0, 2, 1),
+               'MSB of point X', None, None)
 
-    # ECC LSB priv
-    # show_graph(prepare_ecc_priv_lsb_graph('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'LSB of priv key', 500, None)
-    # show_graph(prepare_ecc_priv_lsb_graph('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'LSB of priv key', None, None)
+    # ECC LSB X
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 63, 65, 1),
+               'LSB of point X', 500, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 63, 65, 1),
+               'LSB of point X', None, None)
+
+    # ECC MSB Y
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 65, 67, 1),
+               'MSB of point Y', 500, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 65, 67, 1),
+               'MSB of point Y', None, None)
+
+    # ECC LSB Y
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', -2, None, 1),
+               'LSB of point Y', 500, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', -2, None, 1),
+               'LSB of point Y', None, None)
+
+    # ECC MSB k
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 0, 2, 2),
+               'MSB of k', 500, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', 0, 2, 2),
+               'MSB of k', None, None)
+
+    # ECC LSB k
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', -2, None, 2),
+               'LSB of k', 500, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv', -2, None, 2),
+               'LSB of k', None, None)
 
     # ECC TIMES
-    # show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'Time to generate key (microseconds)', None, None)
-    # show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'Time to generate key (microseconds)', 500, None)
-    # show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'Time to generate key (microseconds)', None, (840, 990))
-    # show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'), 'Time to generate key (microseconds)', 300, (840, 990))
+    show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'),
+               'Time to generate key (microseconds)', None, None)
+    show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'),
+               'Time to generate key (microseconds)', 500, None)
+    show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'),
+               'Time to generate key (microseconds)', None, (840, 990))
+    show_graph(prepare_ecc_priv_times('/home/kubo/Documents/Magisterske_studium/PV204/zing/eckeys.csv'),
+               'Time to generate key (microseconds)', 300, (840, 990))
 
     # -----------------------------------------------------------------------------------------------------------------
 
     # RSA 512 P prime MSB
-    # show_graph(prepare_p_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'MSB P', 250, None)
-    # show_graph(prepare_p_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'MSB P', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', 2, 4, 3), 'MSB P', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', 2, 4 ,3), 'MSB P', None, None)
 
     # RSA 512 Q prime MSB
-    # show_graph(prepare_q_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'MSB Q', 250, None)
-    # show_graph(prepare_q_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'MSB Q', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', 2, 4, 4), 'MSB Q', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', 2, 4, 4), 'MSB Q', None, None)
 
     # RSA 512 P prime LSB
-    # show_graph(prepare_p_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'LSB P', 250, None)
-    # show_graph(prepare_p_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'LSB P', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', -2, None, 3), 'LSB P', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', -2, None, 3), 'LSB P', None, None)
 
     # RSA 512 Q prime LSB
-    # show_graph(prepare_q_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'LSB Q', 250, None)
-    # show_graph(prepare_q_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'LSB Q', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', -2, None, 4), 'LSB Q', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv', -2, None, 4), 'LSB Q', None, None)
 
     # RSA 512 times
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate (miliseconds)', None, None)
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate (miliseconds)', 500, None)
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate (miliseconds)', None, (0,45))
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate (miliseconds)', 300, (0,45))
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate key (miliseconds)', None, None)
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate key (miliseconds)', 500, None)
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate key (miliseconds)', None, (0,45))
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-512-fin.csv'), 'Time to generate key (miliseconds)', 300, (0,45))
 
     # -----------------------------------------------------------------------------------------------------------------
 
     # RSA 1024 P prime MSB
-    # show_graph(prepare_p_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'MSB P', 250, None)
-    # show_graph(prepare_p_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'MSB P', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', 2, 4, 3), 'MSB P', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', 2, 4, 3), 'MSB P', None, None)
 
     # RSA 1024 Q prime MSB
-    # show_graph(prepare_q_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'MSB Q', 250, None)
-    # show_graph(prepare_q_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'MSB Q', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', 2, 4, 4), 'MSB Q', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', 2, 4, 4), 'MSB Q', None, None)
 
     # RSA 1024 P prime LSB
-    # show_graph(prepare_p_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'LSB P', 250, None)
-    # show_graph(prepare_p_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'LSB P', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', -2, None, 3), 'LSB P', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', -2, None, 3), 'LSB P', None, None)
 
     # RSA 1024 Q prime LSB
-    # show_graph(prepare_q_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'LSB Q', 250, None)
-    # show_graph(prepare_q_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'LSB Q', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', -2, None, 4), 'LSB Q', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv', -2, None, 4), 'LSB Q', None, None)
 
     # RSA 1024 times
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'Time to generate (miliseconds)', None, None)
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'Time to generate (miliseconds)', 500, None)
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'Time to generate key (miliseconds)', None, None)
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-1024-fin.csv'), 'Time to generate key (miliseconds)', 500, None)
 
     # -----------------------------------------------------------------------------------------------------------------
 
     # RSA 2048 P prime MSB
-    # show_graph(prepare_p_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'MSB P', 250, None)
-    # show_graph(prepare_p_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'MSB P', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', 2, 4, 3), 'MSB P', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', 2, 4, 3), 'MSB P', None, None)
 
     # RSA 2048 Q prime MSB
-    # show_graph(prepare_q_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'MSB Q', 250, None)
-    # show_graph(prepare_q_prime_msb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'MSB Q', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', 2, 4, 4), 'MSB Q', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', 2, 4, 4), 'MSB Q', None, None)
 
     # RSA 2048 P prime LSB
-    # show_graph(prepare_p_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'LSB P', 250, None)
-    # show_graph(prepare_p_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'LSB P', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', -2, None, 3), 'LSB P', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', -2, None, 3), 'LSB P', None, None)
 
     # RSA 2048 Q prime LSB
-    # show_graph(prepare_q_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'LSB Q', 250, None)
-    # show_graph(prepare_q_prime_lsb('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'LSB Q', None, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', -2, None, 4), 'LSB Q', 250, None)
+    show_graph(prepare_data('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv', -2, None, 4), 'LSB Q', None, None)
 
     # RSA 2048 times
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'Time to generate (miliseconds)', None, None)
-    # show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'Time to generate (miliseconds)', 500, None)
-    pass
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'Time to generate key (miliseconds)', None, None)
+    show_graph(prepare_rsa_times('/home/kubo/Documents/Magisterske_studium/PV204/project/rsatimes/rsatimes-2048-fin.csv'), 'Time to generate key (miliseconds)', 500, None)
+
 
